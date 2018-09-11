bf806739a422a76c0541ed9992c9e6a943ce3209
Christian Chavez <christiancchavez10@hotmail.com>
2018-09-11 16:42:39 -0500

    fix some styles for to correct a few bugs and better the users experience

--- src/components/PrincipalContainerMovie.js
+++ src/components/PrincipalContainerMovie.js
@@ -1,26 +1,9 @@
 import React, { Component } from 'react';
 import Favorites from './Favorites';
-import favorit from '../Images/favorit.png';
-import favoritToggle from '../Images/movies.png';
 const baseUrlTrailerDefault = 'https://www.youtube.com/embed/';
 const baseUrlImage = 'https://image.tmdb.org/t/p/w500';
 
 class PrincipalContainerMovie extends Component {
-    constructor(){
-        super();
-        this.state = {
-            isFavoritesVisible: false,
-        }
-        this.hideShowFavorites = this.hideShowFavorites.bind(this);
-    }
-
-    hideShowFavorites(){
-        this.setState({
-            isFavoritesVisible: !this.state.isFavoritesVisible, 
-        })  
-    };
-
-    
 
     render(){
         const requestMovie = this.props.movieToShow;
@@ -35,8 +18,7 @@ class PrincipalContainerMovie extends Component {
         }
         return(
             <div className="container" style={containerStyle}>
-                {this.state.isFavoritesVisible && <Favorites changeMovieToShow={this.props.changeMovieToShow} movies={this.props.movies} />}
-                {this.state.isFavoritesVisible ? <img className="container__toggle" onClick={this.hideShowFavorites} src={favoritToggle} alt=""></img> : <img className="moveLeft" onClick={this.hideShowFavorites} src={favoritToggle} alt=""></img>}
+                <Favorites changeMovieToShow={this.props.changeMovieToShow} movies={this.props.movies} />
                 <div className="container-info">
                     <span className="container-info__title">{requestMovie.title}</span>
                     <div className="container-info__description">
@@ -44,7 +26,6 @@ class PrincipalContainerMovie extends Component {
                             {requestMovie.overview}
                         </p>
                         <span className="container-info__description-date">{requestMovie.release_date && requestMovie.release_date.substr(0, 4)}</span>
-                        <img className="container-info__description-favorite"  src={favorit} alt=""/>
                     </div> 
                     {requestMovie.videos && requestMovie.videos.results[0] ? <iframe className="container-info__video" title="Movie's trailer" src={baseUrlTrailerDefault + (requestMovie.videos && requestMovie.videos.results[0].key)}  allow="autoplay; encrypted-media" ></iframe> : <p style={iframeStyle}>This movie doesn't have a trailer</p>}
                 </div> 
